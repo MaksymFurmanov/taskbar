614f8f83c511e9acdd2d1b31bcf541644e7a4bff
Maksym Furmanov <maksym.furmanov@student.tuke.sk>
2023-09-03 17:06:53 +0200

    Working task bar with reverse ordering issues

---
+++ src/components/TaskList/reduser.js
@@ -0,0 +1,38 @@
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case ('add'):
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    done: false
+                }
+            ];
+        case ('edit'):
+            return state.map((task) =>
+                task.id === action.id ? {...task, text: action.text} : task
+            );
+        case ('delete'):
+            return state.filter((t) => t.id !== action.id);
+        case ('checked'):
+            return state.map((task) =>
+                task.id === action.id ? {...task, done: !task.done} : task
+            );
+        case ('sort'):
+            let newState = state;
+            newState.sort((a, b) => {
+                let fa = a.text.toLowerCase(), fb = b.text.toLowerCase();
+                if (fa > fb) {
+                    return action.sortType ? -1 : 1;
+                } else if (fa > fb) {
+                    return action.sortType ? 1 : -1;
+                }
+                return 0;
+            })
+            newState.forEach((obj) => console.log(obj.text));
+            return newState;
+        default:
+            return state;
+    }
+}
